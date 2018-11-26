6b1d506d286d380432c1c07c81b7e415899e124e
Sourav Chakraborty <sourovchakraborty420@gmail.com>
2018-11-26 19:33:30 +0600

    Update geo teamplate.cpp

--- geo teamplate.cpp
+++ geo teamplate.cpp
@@ -31,3 +31,32 @@ double func(ll x1,ll y1,ll x2,ll y2,ll x3,ll y3)
 {
     return abs(x1*(y2-y3)+x2*(y3-y1)+x3*(y1-y2))/2.0;
 }
+
+// The main function that returns true if line segment 'p1q1'
+// and 'p2q2' intersect.
+struct Point
+{ double x,y; };
+bool onSegment(Point p, Point q, Point r)
+{
+  if (q.x <= max(p.x, r.x) && q.x >= min(p.x, r.x) && q.y <= max(p.y, r.y) && q.y >= min(p.y, r.y))return true;
+    return false;
+}
+int orientation(Point p, Point q, Point r)
+{
+    int val = (q.y - p.y) * (r.x - q.x) -(q.x - p.x) * (r.y - q.y);
+    if (val == 0) return 0;  // colinear
+    return (val > 0)? 1: 2; // clock or counterclock wise
+}
+bool doIntersect(Point p1, Point q1, Point p2, Point q2)
+{
+    int o1 = orientation(p1, q1, p2);
+    int o2 = orientation(p1, q1, q2);
+    int o3 = orientation(p2, q2, p1);
+    int o4 = orientation(p2, q2, q1);
+    if (o1 != o2 && o3 != o4) return true;
+    if (o1 == 0 && onSegment(p1, p2, q1)) return true;
+    if (o2 == 0 && onSegment(p1, q2, q1)) return true;
+    if (o3 == 0 && onSegment(p2, p1, q2)) return true;
+    if (o4 == 0 && onSegment(p2, q1, q2)) return true;
+    return false;
+}
