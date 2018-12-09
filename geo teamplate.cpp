1bf6a1ed7e2bf886c85c61b88549bbdaca21674c
Sourav Chakraborty <sourovchakraborty420@gmail.com>
2018-12-09 08:22:13 +0600

    Update geo teamplate.cpp

--- geo teamplate.cpp
+++ geo teamplate.cpp
@@ -134,3 +134,27 @@ void func(long double a,long double b,long double c)
     cout<<fixed<<setprecision(10)<<ans<<endl;
     return;
 }
+//monotone chain convexhull
+typedef pair<ll, ll> point;
+bool cw(const point &a, const point &b, const point &c)
+{
+    return (b.first - a.first) * (c.second - a.second) -
+           (b.second - a.second) * (c.first - a.first) < 0;
+}
+vector<point> convexHull(vector<point> p)
+{
+    int n = p.size();
+    if (n <= 1)
+        return p;
+    sort(p.begin(), p.end());
+    int cnt = 0;
+    vector<point> q(n * 2);
+    for (int i = 0; i < n; q[cnt++] = p[i++])
+        for (; cnt >= 2 && !cw(q[cnt - 2], q[cnt - 1], p[i]);
+                --cnt);
+    for (int i = n - 2, t = cnt; i >= 0; q[cnt++] = p[i--])
+        for (; cnt > t && !cw(q[cnt - 2], q[cnt - 1],
+                              p[i]); --cnt);
+    q.resize(cnt - 1 - (q[0] == q[1]));
+    return q;
+}
