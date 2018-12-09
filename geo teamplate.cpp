79b52328ec0a51e36c4e60fb5d397b20ac65f46a
Sourav Chakraborty <sourovchakraborty420@gmail.com>
2018-12-09 12:27:07 +0600

    Update geo teamplate.cpp

--- geo teamplate.cpp
+++ geo teamplate.cpp
@@ -1,9 +1,50 @@
 double: input lf and output f
 long double:take input and output in double.In middle type cast into long double
 #define pi acos(-1)
+
 ellipse:
 area=pi*a*b
 2*a=r1+r2 (r1 and r2 is the distance from foci1 and foci2)
+
+//give two points and radius in func and it return center of the circle     
+vector<pair<double,double> >v;
+double func(double a,double b,double c)
+{
+    double x=-b + sqrt(b*b - 4*a*c);
+    x/=(2*a);
+    return x;
+}
+double func1(double a,double b,double c)
+{
+    double x=-b - sqrt(b*b - 4*a*c);
+    x/=(2*a);
+    return x;
+}
+//you have to call this function two times but 2nd time swap (x1,y1) and (x2,y2)
+//you also have to swap the centers for the 2nd time
+void func(double x1,double y1,double x2,double y2,double radius)
+{
+    if((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)>radius*radius*4)
+        return;
+    double midx1=(x1+x2)/2;
+    double midy1=(y1+y2)/2;
+    if(y1==y2)
+        return ;
+    double tan1=(x2-x1)/(-y2+y1);
+    double x,y;
+    double c=midy1-midx1*tan1;
+    double a=1+tan1*tan1;
+    double b= -2*x1 + 2*c*tan1 - tan1*2*y1;
+    double d= x1*x1 + y1*y1 + c*c - 2*y1*c - radius*radius;
+    x=func(a,b,d);
+    y=c + x*tan1;
+    v.push_back(make_pair(x,y));//swap (x,y) for the 2nd time
+    x=func1(a,b,d);
+    y=c + x*tan1;
+    v.push_back(make_pair(x,y));//swap (x,y) for the 2nd time
+    return;
+}    
+
 #give 3 points and it give u the center fo the circle
 void func(double x1,double y1,double x2,double y2,double x3,double y3)
 {
@@ -20,22 +61,28 @@ void func(double x1,double y1,double x2,double y2,double x3,double y3)
     r=sqrt(r);
     cout<<x<<" "<<y<<" "<<r<<endl;
     }
+
 //the polygon's area 
 Area=1/2*abs((x0*y1-x1*y0)+(x1*y2-x2*y1)...+(xn*yn-1-xn-1*yn)+(xn*y0-x0*yn))
+    
  //orientation check
  ll func(ll x1,ll y1,ll x2,ll y2,ll x,ll y)
 {
     return (x2-x1)*(y-y1) - (y2-y1)*(x-x1);
 } 
+
 //pick's theorem
 area=(boundary_point)/2+inner_point-1
+
 // number of integer points between two points in 2d grid
- GCD(abs(x1 - x2), abs(y1 - y2))
+GCD(abs(x1 - x2), abs(y1 - y2))
+    
 //area when u know three point of triangle
 double func(ll x1,ll y1,ll x2,ll y2,ll x3,ll y3)
 {
     return abs(x1*(y2-y3)+x2*(y3-y1)+x3*(y1-y2))/2.0;
 }
+
 // The main function that returns true if line segment 'p1q1'
 // and 'p2q2' intersect.
 struct Point
@@ -66,6 +113,7 @@ bool doIntersect(Point p1, Point q1, Point p2, Point q2)
     if (o4 == 0 && onSegment(p2, q1, q2)) return true;
     return false;
 }
+
 //Rotating_calipers
 double cross(const point &a, const point &b, const point &c)
 {
@@ -93,6 +141,7 @@ double Rotating_calipers()
         temp/=2;
         printf("%.2f\n",temp);   
 }
+
 //area for intersection of two circle
 // long double a=r1;
 // long double b=r2;
@@ -134,6 +183,7 @@ void func(long double a,long double b,long double c)
     cout<<fixed<<setprecision(10)<<ans<<endl;
     return;
 }
+
 //monotone chain convexhull
 typedef pair<ll, ll> point;
 bool cw(const point &a, const point &b, const point &c)
