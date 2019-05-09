73ce724c2477a3e99fcb327e82f7f1154ffeae53
Sourav Chakraborty <sourovchakraborty420@gmail.com>
2019-05-09 15:24:50 +0600

    Update geo teamplate.cpp

--- geo teamplate.cpp
+++ geo teamplate.cpp
@@ -84,6 +84,37 @@ double func(ll x1,ll y1,ll x2,ll y2,ll x3,ll y3)
     return abs(x1*(y2-y3)+x2*(y3-y1)+x3*(y1-y2))/2.0;
 }
 
+// The main function that returns x,y point where
+//line  'AB' and 'CD' intersect.
+#define pdd pair<double, double>
+pdd lineLineIntersection(pdd A, pdd B, pdd C, pdd D)
+{
+    // Line AB represented as a1x + b1y = c1
+    double a1 = B.second - A.second;
+    double b1 = A.first - B.first;
+    double c1 = a1*(A.first) + b1*(A.second);
+
+    // Line CD represented as a2x + b2y = c2
+    double a2 = D.second - C.second;
+    double b2 = C.first - D.first;
+    double c2 = a2*(C.first)+ b2*(C.second);
+
+    double determinant = a1*b2 - a2*b1;
+
+    if (determinant == 0)
+    {
+        // The lines are parallel. This is simplified
+        // by returning a pair of FLT_MAX
+        return make_pair(FLT_MAX, FLT_MAX);
+    }
+    else
+    {
+        double x = (b2*c1 - b1*c2)/determinant;
+        double y = (a1*c2 - a2*c1)/determinant;
+        return make_pair(x, y);
+    }
+}
+
 // The main function that returns true if line segment 'p1q1'
 // and 'p2q2' intersect.
 struct Point
