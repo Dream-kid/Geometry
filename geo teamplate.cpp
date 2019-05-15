47186f60f8259e270339aee131a781e5053befad
Sourav Chakraborty <sourovchakraborty420@gmail.com>
2019-05-16 01:03:39 +0600

    Update geo teamplate.cpp

--- geo teamplate.cpp
+++ geo teamplate.cpp
@@ -84,6 +84,67 @@ double func(ll x1,ll y1,ll x2,ll y2,ll x3,ll y3)
     return abs(x1*(y2-y3)+x2*(y3-y1)+x3*(y1-y2))/2.0;
 }
 
+//Find Simple Closed Path for a given set of points
+struct Point
+{
+    int x, y;
+};
+// A global point needed for  sorting points with reference
+// to the first point. Used in compare function of qsort()
+Point p0;
+int swap(Point &p1, Point &p2)
+{
+    Point temp = p1;
+    p1 = p2;
+    p2 = temp;
+}
+int dist(Point p1, Point p2)
+{
+    return (p1.x - p2.x)*(p1.x - p2.x) +(p1.y - p2.y)*(p1.y - p2.y);
+}
+int orientation(Point p, Point q, Point r)
+{
+    int val = (q.y - p.y) * (r.x - q.x) -(q.x - p.x) * (r.y - q.y);
+    if (val == 0)return 0;
+    return (val > 0)? 1: 2;
+}
+// A function used by library function qsort() to sort
+//  an array of points with respect to the first point
+int compare(const void *vp1, const void *vp2)
+{
+    Point *p1 = (Point *)vp1;
+    Point *p2 = (Point *)vp2;
+    int o = orientation(p0, *p1, *p2);
+    if (o == 0)
+        return (dist(p0, *p2) >= dist(p0, *p1))? -1 : 1;
+    return (o == 2)? -1: 1;
+}
+Point arr[100000];
+void put_value(Point points[], int n)
+{
+    // Find the bottommost point
+    int ymin = points[0].y, min = 0;
+    for (int i = 1; i < n; i++)
+    {
+        int y = points[i].y;
+        // Pick the bottom-most. In case of tie, chose the
+        // left most point
+        if ((y < ymin) || (ymin == y &&points[i].x < points[min].x))
+            ymin = points[i].y, min = i;
+    }
+    // Place the bottom-most point at first position
+    swap(points[0], points[min]);
+    // Sort n-1 points with respect to the first point.
+    // A point p1 comes before p2 in sorted ouput if p2
+    // has larger polar angle (in counterclockwise
+    // direction) than p1
+    p0 = points[0];
+    qsort(&points[1], n-1, sizeof(Point), compare);
+    for (int i=0; i<n; i++)
+        arr[i].x= points[i].x,arr[i].y= points[i].y;
+    return;
+}
+
 // The main function that returns x,y point where
 //line  'AB' and 'CD' intersect.
 #define pdd pair<double, double>
